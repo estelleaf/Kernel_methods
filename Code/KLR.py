474c7201d71de3b5e00832dbbb034ad221408ba1
Estelle Aflalo <estelleaflalo@eduroam-1-112.enst.fr>
2017-02-13 18:07:07 +0100

    KLR

--- Code/KLR.py
+++ Code/KLR.py
@@ -9,6 +9,7 @@ import math
 import numpy as np
 import pandas as pd
 
+import kernel_functions
 
 X_tr_path='/Users/estelleaflalo/Desktop/M2_Data_Science/Second_Period/Kernel_Methods/Project/Xtr.csv'
 Y_tr_path='/Users/estelleaflalo/Desktop/M2_Data_Science/Second_Period/Kernel_Methods/Project/Ytr.csv'
@@ -20,8 +21,11 @@ df_y=pd.read_csv(Y_tr_path, header=None)
 
 X=df_X.as_matrix()
 y=df_y.as_matrix()[:,1]
-y=y[1:].astype(float)
+y=y[1:]
+y=y.astype(float)
 
+def logistic(u):
+    return np.log(1./(1+np.exp(-u)))
 
 def log_prime(u):
     return -1./(1+np.exp(u))
@@ -30,8 +34,8 @@ def log_primeprime(u):
     return 1.*np.exp(u)/(1+np.exp(u))**2
 
 class KLR():
-    def __init__(self,kernel,alpha_init,lamda):
-        self.ker=kernel
+    def __init__(self,alpha_init,lamda):#,kernel):
+        #self.ker=kernel
         self.alpha0=alpha_init
         self.n=alpha_init.shape[0]
         self.lamb=lamda
@@ -39,26 +43,29 @@ class KLR():
         
         
     def fit(self, X, y):
-        K=self.ker(X)
-
+        #K=self.ker(X)
+        K=X.dot(np.transpose(X))
         temp=(K.dot(self.alpha0))*y
         P=np.diag(log_prime(temp))
         W=np.diag(log_primeprime(temp))
         Z=K.dot(alpha0)-np.linalg.inv(W).dot(P).dot(y)
-        alpha=alpha0
         for i in range(10):
             alpha=np.linalg.inv(np.transpose(K).dot(W).dot(K)+self.n*self.lamb*K).dot(np.transpose(Z).dot(W).dot(K))
             m=K.dot(alpha)
             P=np.diag(log_prime(m))
             W=np.diag(log_primeprime(m))
-            Z=m-np.linalg.inv(W).dot(P*y)
+            Z=m-np.linalg.inv(W).dot(P).dot(y)
             J=(1./self.n)*(np.transpose(K.dot(alpha)-Z).dot(W).dot(K.dot(alpha)-Z))+self.lamb*np.transpose(alpha).dot(K).dot(alpha)
             print i, J
+        return alpha
         
         
         
-    def predict(self, X):
-        return self.clf.predict(X)
+    def fit_predict(self, Xtr,Ytr,Xte):
+        K=self.ker(Xte)
+        return K.dot(self.fit(Xtr,Ytr))
+        
+        
 
     def predict_proba(self, X):
         return self.clf.predict_proba(X)
